9bf84340e6b87d74acf729c6abf081fef1676ce3
PoullainThomas <102023981+PoullainThomas@users.noreply.github.com>
2022-03-28 15:25:40 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 it('renders learn react link', () => {
   mount(<App />);
-  cy.get('a').contains('Learn React');
+  cy.get('a').contains(/Learn React/i);
 });
 
 it('count initialized with 0', () => {
