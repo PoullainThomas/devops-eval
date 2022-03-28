e8a04a616b111add8ca6bc878353b0efe5c48786
PoullainThomas <102023981+PoullainThomas@users.noreply.github.com>
2022-03-28 21:30:42 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,17 +2,9 @@ import React from 'react';
 import { mount } from '@cypress/react';
 import App from './App';
 
-it('renders learn react link', () => {
-  mount(<App />);
-  cy.get('a').contains(/Learn react/i);
-});
 
-it('count initialized with 0', () => {
-  mount(<App />);
-  cy.get('[data-cy="count"]').contains('0');
-})
-it('button click increase count by 1', () => {
-  mount(<App />);
-  cy.get('[data-cy="button"]').click();
-  cy.get('[data-cy="count"]').contains('1');
-})
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
